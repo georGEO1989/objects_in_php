4c5d484433d863bab78e07bdceb1cf5867e3d9eb
George Offley <goffley3@aol.com>
2015-05-17 21:20:12 -0400

    added html for results

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+	<title>Check for Spam</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<h2>Let's find some spam</h2>	
+	<form class="form" action="objects.php" method="post">
+		<div class="form-group">
+			<label>Name</label>
+			<input class="form-control" type="text" name="name"><br>
+		</div>
+		<div class="form-group">
+			<label>Email</label>
+			<input class="form-control" type="text" name="email"><br>
+		</div>
+		<div class="form-group">
+			<label>Password</label>
+			<input class="form-control" type="text" name="password"><br>
+		</div>
+		<div class="form-group">
+			<label>From</label>
+			<input class="form-control" type="text" name="from">
+		</div>
+		<button type="submit" class="btn btn-default">Submit</button>
+	</form>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
--- objects.php
+++ objects.php
@@ -1,7 +1,32 @@
 <?php
-include_once "input.php";
+$hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
+$username = $_POST['email'];
+$password = $_POST['password'];
+$from = $_POST['from'];
 
-function checkString($this){ #function object
+
+$inbox = imap_open($hostname, $username, $password) or die('Cannot connect');
+
+$emails = imap_search($inbox,'FROM "@'. $from . '"');
+
+if($emails) {
+	$output = '';
+
+	rsort($emails);
+
+	foreach($emails as $email_number) {
+		$overview = imap_fetch_overview($inbox, $email_number,0);
+		$message = imap_fetchbody($inbox, $email_number,2);
+		$output.='<div class="body">'. $message . "</div>";
+	}
+
+	//echo $output;
+}
+
+imap_close($inbox);
+/////////////////////////////////////////////////////////////////////////////////
+// Function for checking for spam
+function checkString($this){
 	//filter for the spam list
 	$filter_list = array();
 	array_push($filter_list, "email",
@@ -13,11 +38,15 @@ function checkString($this){ #function object
 	foreach ($filter_list as $key) {
 		//this to search through the string and find the keywords
 		if (strpos($this, $key) !== false) {
-			echo 'true';
+			header( 'Location: http://localhost/objects/result_bad.html' );
 		} 
+		else {
+			header( 'Location: http://localhost/objects/result_good.html' );
+		}
 	}
 }
 
 // $string = "This is totally spam";
 
 checkString($output);
+?>
